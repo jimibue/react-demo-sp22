1fd2d5767dbf4f6db24c365704c8a161326c1e77
James <jimibue@gmail.com>
2022-05-23 12:05:57 -0600

    card component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 import Card from "./Card";
 import CardInline from "./CardInline";
 import JSXDemo from "./JSXDemo";
+let users = [
+  {id:1, name:"Bob", about:'likes to Fish'},
+  {id:2, name:"Jill", about:'likes to Swim'},
+  {id:3, name:"George", about:'likes to Code and Paint'},
+  {id:4, name:"Sally", about:'likes to Paint'},
+]
 
 function App() {
+  const renderUsers = ()=>{
+      return users.map((user)=>{
+        return <Card key={user.id} name={user.name} about={user.about} />
+      })
+  }
+
   return (
     <>
       <h1>App</h1>
-      <Card />
-      <CardInline />
+      {/* <Card name='Bob' about='likes to fish' />
+      <Card name='Jill' about='likes to swim'/> */}
+      {renderUsers()}
+  
       {/* <JSXDemo /> */}
     </>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,11 @@
-const Card = () => {
+// props is always going to be an object {}
+
+const Card = (props) => {
   return (
       <div className="container">
           <div className="bio">
-          <h3 className="user-name">User name</h3>
-          <p>about user</p>
+          <h3 className="user-name">{props.name || 'no name prop given'}</h3>
+          <p>{props.about || 'no about prop given'}</p>
           </div>
       </div>
   )
